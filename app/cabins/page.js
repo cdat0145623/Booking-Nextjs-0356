bc9cd2c2867956386aadfead9d6226d11c0ec64e
NguyenChauQuocDat <cdat789@gmail.com>
2025-08-14 21:48:12 +0700

    Updated

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,20 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Counter from "../_components/Counter";
 import Filter from "../_components/Filter";
 import ReservationReminder from "../_components/ReservationReminder";
 
 export const revalidate = 3600;
+// export const revalidate = 15;
 
 export const metadata = {
-    title: "Cabin",
+    title: "Cabins",
 };
 
-export default async function Page({ searchParams }) {
+export default function Page({ searchParams }) {
     const filter = searchParams?.capacity ?? "all";
-    console.log(filter);
-    // CHANGE
+
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,10 +25,11 @@ export default async function Page({ searchParams }) {
                 Italian Dolomites. Imagine waking up to beautiful mountain
                 views, spending your days exploring the dark forests around, or
                 just relaxing in your private hot tub under the stars. Enjoy
-                nature's beauty in your own little home away from home. The
+                nature&apos;s beauty in your own little home away from home. The
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
-            <div className="flex justify-end my-8">
+
+            <div className="flex justify-end mb-8">
                 <Filter />
             </div>
 
